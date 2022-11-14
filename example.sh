9c96739d7e0985828775999546a1d76162918a5f
onuii-gemma <gemma@onuii.com>
2022-11-14 12:19:30 +0900

    test

--- example.sh
+++ example.sh
@@ -1,5 +1,5 @@
 # echo $ac Kim
 echo shell started
-echo $ORIGIN_MONGO_URI_DEV
+# echo $ORIGIN_MONGO_URI_DEV
 # mongoexport --uri  --collection timeblocks --type json --out exported_timeblocks
 echo shell finished
